270237bf88e4eff7c1683b29bde6bf998c26c386
Paulo <pauloeduardo2709@gmail.com>
2021-05-16 00:37:41 -0300

    Add godotenv and logger

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"github.com/Pauloo27/archvium/logger"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	logger.HandleFatal(err, "Where's the mf .env???")
+}
 
 func main() {
-	fmt.Println("hello world")
 }
