c308519c9830eb847f9debd81b0a9ba64e8fba76
Paulo <pauloeduardo2709@gmail.com>
2021-05-16 11:19:43 -0300

    Add model creation

--- backend/controllers/auth/register.go
+++ backend/controllers/auth/register.go
@@ -3,7 +3,8 @@ package auth
 import (
 	"net/http"
 
-	"github.com/Pauloo27/archvium/logger"
+	"github.com/Pauloo27/archvium/model"
+	"github.com/Pauloo27/archvium/services/db"
 	"github.com/Pauloo27/archvium/utils"
 	"github.com/gofiber/fiber/v2"
 )
@@ -16,7 +17,18 @@ type RegisterPayload struct {
 
 func Register(ctx *fiber.Ctx) error {
 	payload := ctx.Locals("payload").(*RegisterPayload)
+	newUser := model.User{
+		Email: payload.Email, Password: payload.Password, Username: payload.Username,
+	}
 
-	logger.Successf("%v", payload)
-	return utils.AsMsg(ctx, http.StatusCreated, "nice")
+	err := db.Connection.Create(&newUser).Error
+	if err != nil {
+		if utils.IsNotUnique(err) {
+			return utils.AsError(ctx, http.StatusConflict, err.Error())
+		} else {
+			return utils.AsError(ctx, http.StatusInternalServerError, err.Error())
+		}
+	}
+
+	return utils.AsJSON(ctx, http.StatusCreated, fiber.Map{"id": newUser.ID})
 }
