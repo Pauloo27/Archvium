be7f2f21baa189fcdd599b6327e79c748d6a71b0
Paulo <pauloeduardo2709@gmail.com>
2021-05-18 00:25:53 -0300

    Add router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <h1>hello</h1>
+    <Router>
+      <h1>nav</h1>
+      <Switch>
+        <Route path="/" exact>
+          <h1>home</h1>
+        </Route>
+        <Route path="/login" exact >
+          <h1>login</h1>
+        </Route>
+        <Route path="/register" exact >
+          <h1>register</h1>
+        </Route>
+        <Route>
+          <h1>404</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
