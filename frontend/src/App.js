649777a7d06c76ad33c0247286ba1951636e99e1
Paulo <pauloeduardo2709@gmail.com>
2021-05-18 00:20:26 -0300

    Improve basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 
-export default () => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+export default function App() {
+  return (
+    <h1>hello</h1>
+  );
+}
