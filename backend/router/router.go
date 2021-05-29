5ee87f5cf1d9688d0af0618d25511e3a0de548e0
Paulo <pauloeduardo2709@gmail.com>
2021-05-28 23:40:19 -0300

    Add /users/@me route

--- backend/router/router.go
+++ backend/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	authController "github.com/Pauloo27/archvium/controllers/auth"
+	usersController "github.com/Pauloo27/archvium/controllers/users"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -9,6 +10,7 @@ func RouteFor(app *fiber.App) {
 
 	v1 := app.Group("/v1")
 	auth := v1.Group("/auth")
+	users := v1.Group("/users")
 
 	auth.Post("/register",
 		requireGuest,
@@ -21,4 +23,6 @@ func RouteFor(app *fiber.App) {
 		withPayload(new(authController.LoginPayload)),
 		authController.Login,
 	)
+
+	users.Get("/@me", requireAuth, usersController.GetMe)
 }
