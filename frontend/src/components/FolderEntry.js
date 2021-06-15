4ca89222afdc73a75e283aab9e66b57a98fa94de
Paulo <pauloeduardo2709@gmail.com>
2021-06-14 21:07:58 -0300

    Add Material Icons

--- frontend/src/components/FolderEntry.js
+++ frontend/src/components/FolderEntry.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/FolderEntry.css";
 
 export default function FolderEntry({ file }) {
   const path = file.path.split("/").slice(2).join("/");
   if (file.isDir) {
     return (
-      <Link to={path}>
-        <p>
-          &gt;
+      <Link to={path} className="folder-entry-link">
+        <p className="folder-entry">
+          <span className="material-icons-outlined folder-entry-icon">
+            folder
+          </span>
           {file.name}
         </p>
       </Link>
     );
   }
   return (
-    <p>
-      *
+    <p className="folder-entry">
+      <span className="material-icons-outlined folder-entry-icon">
+        description
+      </span>
       {file.name}
     </p>
   );
