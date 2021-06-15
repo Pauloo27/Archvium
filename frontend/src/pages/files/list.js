62c17e6af202795d34d5ac4c230d70137ecfc6dd
Paulo <pauloeduardo2709@gmail.com>
2021-06-14 21:36:58 -0300

    Add folders navigator

--- frontend/src/pages/files/list.js
+++ frontend/src/pages/files/list.js
@@ -4,6 +4,7 @@ import { doAuthedRequest as doRequest } from "../../api/core";
 import useAuth from "../../hooks/auth";
 import Page404 from "../404";
 import FolderEntry from "../../components/FolderEntry";
+import FolderPath from "../../components/FolderPath";
 
 export default function PageFilesList() {
   const route = useRouteMatch();
@@ -19,7 +20,11 @@ export default function PageFilesList() {
     setFiles(undefined);
     doRequest(`/folders/index/${user.username}/${path}`, {})
       .then((res) => {
-        if (res.status === 404 || res.status === 400) { return setFiles(null); }
+        if (res.status === 404 || res.status === 400) {
+          setFiles(null);
+          return;
+        }
+
         if (res.status === 200) res.json().then(setFiles);
       });
   }, [user, path, setFiles]);
@@ -29,12 +34,7 @@ export default function PageFilesList() {
 
   return (
     <>
-      <h1>
-        Files at
-        {" "}
-        /
-        {path}
-      </h1>
+      <FolderPath path={path} />
       <ul>
         {files.map((file) => (
           <li key={file.name}>
